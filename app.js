762f9e25eb84dde5b052192fe3aaad8b8f3274e1
Tareq Uddin Mahmud <djuice18@gmail.com>
2018-05-27 23:14:45 +0300

    last commit

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
-const express= require('express');
+const express = require("express");
 
 const app = express();
-const morgan = require('morgan');
-const bodyParser=require('body-parser')
-const cors = require('cors')
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-
 
 mongoose.connect("mongodb://TM:123456@ds227740.mlab.com:27740/shop-rest-api");
 
-const uploadRoutes= require('./api/routes/uploads')
+const uploadRoutes = require("./api/routes/uploads");
 
-app.use(cors())
-app.use(morgan('dev'));
+app.use(cors());
+app.use(morgan("dev"));
 app.use("/uploads", express.static("uploads"));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -31,22 +30,19 @@ app.use((req, res, next) => {
   }
   next();
 });
-// Our upload routes added here 
-app.use('/uploads', uploadRoutes);
+// Our upload routes added here
+app.use("/uploads", uploadRoutes);
 
-
-
-app.use((req, res, next)=>{
-const error= new Error('its all empty here')
-error.status= 404;
-next(error)
+app.use((req, res, next) => {
+  const error = new Error("its all empty here");
+  error.status = 404;
+  next(error);
 });
 
-
-app.use((error, req, res, next)=>{
-res.status(error.status|| 500)
-res.json({
-    error:{message:error.message}
-})
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: { message: error.message }
+  });
 });
-module.exports = app
+module.exports = app;
